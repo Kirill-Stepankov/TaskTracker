000defcb400cb8df79ea59aff2972d9de560fc75
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-09-02 14:41:39 +0300

    custom user model

--- tasktracker/tasktracker/settings.py
+++ tasktracker/tasktracker/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'userprofile'
 ]
 
 MIDDLEWARE = [
@@ -112,6 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'userprofile.Profile'
 
 
 # Internationalization
---
+++ tasktracker/userprofile/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Username is required.")
+        if not email:
+            raise ValueError("An email is required.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Username is required.")
+        if not email:
+            raise ValueError("An email is required.")
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        user = self.create_user(username, email, password, **extra_fields)
+        user.save()
+        return user
+
+class Profile(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    city = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
