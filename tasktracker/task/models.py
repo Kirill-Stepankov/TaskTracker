062f76eba67a29c9cc727a731108641925dca28a
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-09-03 21:11:10 +0300

    task model

--- tasktracker/task/models.py
+++ tasktracker/task/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    class StatusChoices(models.TextChoices):
+        TO_DO = 'TD', 'TO DO'
+        IN_PROGRESS = 'IP', 'IN PROGRESS'
+        DONE = 'DN', 'DONE'
+
+    class PriorityChoices(models.TextChoices):
+        HIGH = 'HG', 'High'
+        MEDIUM = 'MD', 'Medium'
+        LOW = 'LW', 'Low'
+
+    class CategoryChoices(models.TextChoices):
+        WORK = 'WK', 'Work'
+        HOME = 'HM', 'Home'
+        LEARNING = 'LN', 'Learning'
+        OTHER = 'TH', 'Other'
+
+    description = models.TextField()
+    status = models.CharField(max_length=2, choices=StatusChoices.choices, default=StatusChoices.TO_DO)
+    priority = models.CharField(max_length=2, choices=PriorityChoices.choices, default=PriorityChoices.LOW)
+    category = models.CharField(max_length=2, choices=CategoryChoices.choices, default=CategoryChoices.HOME)
+    date_created = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField()
--- tasktracker/tasktracker/settings.py
+++ tasktracker/tasktracker/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'userprofile'
+    'userprofile',
+    'task'
 ]
 
 MIDDLEWARE = [
