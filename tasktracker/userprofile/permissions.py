df629efbe8eb07567fe64c72d665c5badc13e2c9
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-09-03 20:33:38 +0300

    task app

--- tasktracker/userprofile/permissions.py
+++ tasktracker/userprofile/permissions.py
@@ -9,7 +9,4 @@ class IsAdminOrIsSelf(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(request.user and request.user.is_staff) or obj == request.user
     
-class IsSelf(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj == request.user
 
