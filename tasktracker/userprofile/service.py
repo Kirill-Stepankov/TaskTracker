4066836a33aaa2e84c21e5993f59774a3b82e4a9
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-09-07 17:11:09 +0300

    delete print statement

--- tasktracker/userprofile/service.py
+++ tasktracker/userprofile/service.py
@@ -1,11 +1,16 @@
 from tasktracker.settings import SECRET_KEY
 import jwt
+from jwt import exceptions
+from rest_framework.exceptions import AuthenticationFailed
 
 class UserService:
     @staticmethod
     def get_user_data(request) -> dict:
         access_token = request.META.get('HTTP_AUTHORIZATION').split()[1]
-        data = jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])
+        try:    
+            data = jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])
+        except exceptions.DecodeError as e:
+            raise AuthenticationFailed(code=401, detail=str(e))
         return data
     
     def _get_value_from_jwt(self, request, key) -> str:
