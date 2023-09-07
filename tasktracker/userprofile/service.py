214a8d5926ed4f2decf8b2aba76875bc30676021
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-09-07 11:43:09 +0300

    delete print statement

--- tasktracker/userprofile/service.py
+++ tasktracker/userprofile/service.py
@@ -4,7 +4,6 @@ import jwt
 class UserService:
     @staticmethod
     def get_user_data(request) -> dict:
-        print(request.META)
         access_token = request.META.get('HTTP_AUTHORIZATION').split()[1]
         data = jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])
         return data
