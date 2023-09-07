110f0be9b7c98726937b1d1fd9a25c25acd68f87
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-09-07 11:43:20 +0300

    test get task

--- tasktracker/task/tests/test_task_crud.py
+++ tasktracker/task/tests/test_task_crud.py
@@ -1,11 +1,12 @@
 import pytest
 from rest_framework.test import APIClient
 from rest_framework.permissions import IsAuthenticated
+from task.permissions import IsAdminOrIsOwner
 from tests.conftest import (
     user_jwt,
     access_token
 )
-from userprofile.models import Profile
+from task.models import Task
 
 
 client = APIClient()
@@ -14,6 +15,25 @@ client = APIClient()
 def is_authenticated_mock(mocker):
     mock = mocker.patch.object(IsAuthenticated, 'has_permission')
     return mock
+
+@pytest.fixture
+def is_admin_or_is_self_mock(mocker):
+    mock = mocker.patch.object(IsAdminOrIsOwner, 'has_object_permission')
+    return mock
+
+@pytest.fixture
+def task(
+    task_credentials,
+    user_jwt,
+    access_token,
+):
+    client.credentials(HTTP_AUTHORIZATION='Bearer ' + access_token)
+    response = client.post('/api/v1/tasks/', task_credentials)
+    print(response.content)
+    return Task.objects.filter(owner=user_jwt).first()
+
+
+
 
 
 @pytest.mark.parametrize(
@@ -30,10 +50,36 @@ def test_create_task(
         is_authenticated_mock,
         user_jwt,
         access_token,
-        ):
+):
     is_authenticated_mock.return_value = is_authenticated
 
     client.credentials(HTTP_AUTHORIZATION='Token ' + access_token)
     response = client.post('/api/v1/tasks/', task_credentials)
 
     assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+        "is_admin_or_is_self, expected_status",
+        [
+            (True, 200),
+            (False, 401)
+        ]
+)
+def test_get_task(
+        is_admin_or_is_self_mock,
+        is_admin_or_is_self,
+        expected_status,
+        user_jwt,
+        access_token,
+        task,
+):
+    is_admin_or_is_self_mock.return_value = is_admin_or_is_self
+
+    client.credentials(HTTP_AUTHORIZATION='Token ' + access_token)
+
+    response = client.get(f'/api/v1/tasks/{task.pk}/')
+
+    assert response.status_code == expected_status
+
+    
